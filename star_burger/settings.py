7b5e4cd959c9b0066ad07974a740e078be407dd4
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2025-05-28 12:39:53 +0300

    Added Rollbar config(1)

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -14,6 +14,7 @@ API_KEY = env('APIKEY')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 ROLLBAR_ACCESS_TOKEN = env.str('ROLLBAR_ACCESS_TOKEN', None)
+ROLLBAR_ENVIRONMENT = env.str('ROLLBAR_ENVIRONMENT', 'production')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
@@ -47,7 +48,7 @@ MIDDLEWARE = [
 
 ROLLBAR = {
     'access_token': ROLLBAR_ACCESS_TOKEN,
-    'environment': 'development' if DEBUG else 'production',
+    'environment': ROLLBAR_ENVIRONMENT,
     'code_version': '1.0',
     'root': BASE_DIR,
 }
